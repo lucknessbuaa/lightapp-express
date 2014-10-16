7e669289d1fe6443d77e79c5d2dde7355675ed80
jarvys <yuhan534@126.com>
2014-10-16 17:17:13 +0800

    save

--- portal/views.py
+++ portal/views.py
@@ -13,19 +13,19 @@ def login(request):
 
 @login_required
 def sign(request):
-    return render(request, "portal/index.html")
+    return render(request, "portal/sign.html")
 
 
 @login_required
 def send(request):
-    return render(request, "portal/index.html")
+    return render(request, "portal/send.html")
 
 
 @login_required
 def store(request):
-    return render(request, "portal/index.html")
+    return render(request, "portal/store.html")
 
 
 @login_required
 def profile(request):
-    return render(request, "portal/index.html")
+    return render(request, "portal/profile.html")
