a939e5878de75f493273422db97a30acd4eb7a99
jarvys <yuhan534@126.com>
2014-10-17 12:03:53 +0800

    save

--- wsgi.py
+++ wsgi.py
@@ -11,4 +11,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from bae.core.wsgi import WSGIApplication
+application = WSGIApplication(get_wsgi_application())
