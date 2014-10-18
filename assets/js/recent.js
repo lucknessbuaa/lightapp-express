8c6f7a6ed514c10de166370b3a3fa638916ace15
liushuangshuang <liushuangshuang@limijiaoyin.com>
2014-10-18 11:31:33 +0800

    modify the delete function

--- assets/js/recent.js
+++ assets/js/recent.js
@@ -1,15 +1,25 @@
 $(function() {
-    $(".btn.deletePackage").bind("click", function(){
+    $(".deletePackage").bind("click", function(){
         var value = $(this.parentElement.parentElement).find('input[name=orderid]').val();
 
+        $(this).html('正在删除...');
+        $(this).prop('disabled', true);
         $.post("/app/recent/delete",{
             orderid: parseInt(value)
-        }, function(data){
-            if(data.ret_code == 0){
+        }, 'json').then(function(data){
+            if(data.retValue.code === 200){
                 alert('该订单成功删除！');
+            }else if(data.retValue.code === 405) {
+                alert('该订单不存在或已被取消!');
+            }else if(data.retValue.code === 500) {
+                alert('错误，用户不存在或者authcode对应的用户与订单id对应的用户不一致');
+            }else if(data.retValue.code === 710) {
+                alert('该订单已被处理，无法删除');
             }
 
             window.location.reload();
-        }, 'json'); 
+        }, function() {
+            alert('网络异常');
+        }); 
     });
 });
