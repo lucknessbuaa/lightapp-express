e9e3d7cdedf7ccbe173e8015fd2c284c7f4bcbda
jarvys <yuhan534@126.com>
2014-10-21 11:04:04 +0800

    add validators for profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,8 +2,25 @@ $(function() {
     var $form = $('form');
     var $submit = $form.find('input[type=submit]');
     var form = $form[0];
+    var phoneValidator = /^\d{11}$/;
     $form.submit(function(e) {
         e.preventDefault();
+
+        if (form.name.value == "") {
+            return alert("请输入姓名");
+        }
+
+        if (form.phone.value == "") {
+            return alert("请输入手机号码");
+        }
+
+        if (form.address.value == "") {
+            return alert("请输入宿舍地址");
+        }
+
+        if (!phoneValidator.test(form.phone.value)) {
+            return alert("请输入正确格式的手机号码 : )");
+        }
 
         $submit.button('loading');
         $.post('/app/profile', {
